39993978aed73aa6ef6deaf4f6a9157ccd27ef6f
emmy grammy <ojeabuo24@gmail.com>
2026-01-09 00:57:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Form />
+      <ParkingList />
+      <Stats />
     </div>
   );
 }
 
+function Logo(){
+  return(
+    <div className='logo'>
+    <h1> 🌴 Far away 👜 </h1>
+    </div>
+  )
+}
+function Form(){
+  return(
+    <div className='add-form'>
+      <h3> what do you want for your 😍 trip? </h3>
+    </div>
+  )
+}
+function ParkingList(){
+  return(
+    <div className='list'>
+      list 
+    </div>
+  )
+}
+function Stats(){
+  return(
+    <footer className='stats'>
+      <em>
+        💼you have x item on your list and you have already parked x (x%)
+      </em>
+    </footer>
+  )
+}
 export default App;
